8a6f07a49e8928f2160d6939b1ae605b639ab276
ouzor <ouzor@iki.fi>
2014-04-25 08:43:49 +0300

    Moved firstlib.R to /R/, added line changes to startup message

--- R/firstlib.R
+++ R/firstlib.R
@@ -19,6 +19,6 @@
   # Sys.setlocale(locale = "UTF-8")
   # Sys.setlocale(locale = "WINDOWS-1252")
 
-  packageStartupMessage("fingis R package: tools for open GIS data for Finland. This R package is part of rOpenGov <ropengov.github.io>. Copyright (C) 2010-2014 Leo Lahti, Juuso Parkkinen and Joona Lehtomaki. This is free software. You are free to use, modify and redistribute it under the FreeBSD license.")
+  packageStartupMessage("fingis R package: tools for open GIS data for Finland.\nThis R package is part of rOpenGov <ropengov.github.io>.\nCopyright (C) 2010-2014 Leo Lahti, Juuso Parkkinen and Joona Lehtomaki.\nThis is free software. You are free to use, modify and redistribute it under the FreeBSD license.")
 
 }

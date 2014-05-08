ae5d9bdd622f516cc02faa3c21c3537c39db53a2
ouzor <ouzor@iki.fi>
2014-05-08 15:56:26 +0300

    Simplified sp2df implementation, now works with get_hsy("Vaestotietoruudukko")

--- R/visualization.R
+++ R/visualization.R
@@ -17,6 +17,7 @@
 #'
 #' @param sp A spatial object to be transformed
 #' @param region A string specifying the region of interest
+#' @param verbose logical. Should R report extra information on progress? 
 #' @return A ggplot2 theme object
 #' 
 #' @importFrom ggplot2 fortify
@@ -35,15 +36,13 @@
 #'           ggplot(df.suuralue, aes(x=long, y=lat, fill=Name)) + 
 #'           geom_polygon() + theme(legend.position="none")
 
-sp2df <- function(sp, region=NULL) {
-  
-  ## TODO: study how general and/or necessary this function is!
-  # Add IDs 
-  sp@data$id <- rownames(sp@data) 
-  
+sp2df <- function(sp, region=NULL, verbose=TRUE) {
+    
+  if (verbose)
+    message("Transforming ", class(sp), " into a data frame")
   if (class(sp)=="SpatialPointsDataFrame") {
     # Construct data frame manually, as ggplot::fortify can not handle SpatialPoints
-    if (!is.null(region))
+    if (!is.null(region) & verbose)
       message("Note! parameter 'region' not used for SpatialPointsDataFrame")
     df <- data.frame(long=sp@coords[,1], lat=sp@coords[,2], sp@data)
     
@@ -51,16 +50,27 @@ sp2df <- function(sp, region=NULL) {
     if (is.null(region))
       stop("Please specify 'region'!")
     
+    ## NEW implementation 8.5.2014
     # Get point data
     sp.points <- ggplot2::fortify(sp, region=region)
-    # Regex to joinable format
-    sp.points$group <- sub(".1", "", sp.points$group) 
-    # Put everything together
-    df <- merge(sp.points, sp@data, by.x="group", by.y = region) 
-    # sort DF so that polygons come out in the right order
-    df <- df[order(df$order),] 
-    df[[region]] <- df$group
-    df$group <- df$id.x <- df$id <- df$id.y <- NULL 
+    names(sp.points)[names(sp.points)=="id"] <- region
+    # Merge original data
+    df <- merge(sp.points, sp@data, by=region) 
+    
+    ## OLD implementation
+#     # Add IDs 
+#     sp@data$id <- rownames(sp@data) 
+#     # Get point data
+#     sp.points <- ggplot2::fortify(sp, region=region)
+#     # Regex to joinable format
+#     sp.points$group <- sub(".1", "", sp.points$group) 
+#     # Put everything together
+#     df <- merge(sp.points, sp@data, by.x="group", by.y = region, all.x=TRUE) 
+#     
+#     # sort DF so that polygons come out in the right order
+#     df <- df[order(df$order),] 
+#     df[[region]] <- df$group
+#     df$group <- df$id.x <- df$id <- df$id.y <- NULL 
   }
   return(df)  
 }

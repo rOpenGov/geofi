3deb0305871dbdf03188a21a744152981016f083
antagomir <antagomir@lei>
2014-06-03 14:41:11 +0200

    ok

---
+++ R/ip.R
@@ -0,0 +1,31 @@
+
+#' Get geographic coordinates for a given IP-address from 
+#' http://www.datasciencetoolkit.org//ip2coordinates/
+#'
+#' @param ip IP address as character
+#'
+#' @return Latitude and longitude as a numeric vector
+#' @export 
+#' @references
+#' See citation("gisfin") 
+#' @author Leo Lahti \email{louhos@@googlegroups.com}
+#' @note Modified from original version by Kay Cichini
+#' @examples \dontrun{ip_location("137.224.252.10")}
+#' @keywords utilities
+
+ip_location <- function(ip) {
+
+   url <- "http://www.datasciencetoolkit.org//ip2coordinates/"
+   URL_IP <- paste(url, ip, sep = "")
+
+   message(paste("Retrieving the IP", ip, "from ", url))
+
+   api_return <- readLines(URL_IP, warn = F)
+   lon1 <- api_return[grep("longitude", api_return)]
+   lon <- gsub("[^[:digit:].]", "", lon1)
+   lat1 <- api_return[grep("latitude", api_return)]
+   lat <- gsub("[^[:digit:].]", "", lat1)
+   return(c(lat, lon))
+
+}
+

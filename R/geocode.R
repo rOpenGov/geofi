95664a313a935ecb7b9db71d50284f4a99102ff0
antagomir <leo.lahti@iki.fi>
2015-06-06 19:14:57 +0300

    Added municipality visualization functions

--- R/geocode.R
+++ R/geocode.R
@@ -1,18 +1,3 @@
-# This file is a part of the helsinki package (http://github.com/rOpenGov/helsinki)
-# in association with the rOpenGov project (ropengov.github.io)
-
-# Copyright (C) 2010-2014 Juuso Parkkinen / Louhos <louhos.github.com>. 
-# All rights reserved.
-
-# This program is open source software; you can redistribute it and/or modify 
-# it under the terms of the FreeBSD License (keep this notice): 
-# http://en.wikipedia.org/wiki/BSD_licenses
-
-# This program is distributed in the hope that it will be useful, 
-# but WITHOUT ANY WARRANTY; without even the implied warranty of 
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
-
 #' Get geocode for given query
 #'
 #' Get gecode for given query from one of the geocoding services:

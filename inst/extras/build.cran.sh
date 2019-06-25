b9c1a1687596d06ae340babace6f31b3234498eb
antagomir <leo.lahti@iki.fi>
2019-06-25 16:04:01 +0300

    documentation and first build ok

--- inst/extras/build.cran.sh
+++ inst/extras/build.cran.sh
@@ -1,4 +1,4 @@
-/home/lei/bin/R CMD BATCH document.R
-/home/lei/bin/R CMD build ../../ # --no-build-vignettes
-/home/lei/bin/R CMD check --as-cran gisfin_0.10.9000.tar.gz
-/home/lei/bin/R CMD INSTALL gisfin_0.10.9000.tar.gz
+#/home/lei/bin/R CMD BATCH document.R
+~/bin/R-3.6.0/bin/R CMD build ../../ # --no-build-vignettes
+~/bin/R-3.6.0/bin/R CMD check --as-cran gisfin_0.9.27.9000.tar.gz
+~/bin/R-3.6.0/bin/R CMD INSTALL gisfin_0.9.27.9000.tar.gz

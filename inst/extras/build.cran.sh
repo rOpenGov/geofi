d97f6b3cc5344790e157d6ec95226e765bd9d322
antagomir <leo.lahti@iki.fi>
2019-06-25 15:35:08 +0300

    Version number updated to build.cran.sh script

--- inst/extras/build.cran.sh
+++ inst/extras/build.cran.sh
@@ -1,4 +1,4 @@
-/usr/bin/R CMD BATCH document.R
-/usr/bin/R CMD build ../../ # --no-build-vignettes
-/usr/bin/R CMD check --as-cran gisfin_0.9.27.tar.gz
-/usr/bin/R CMD INSTALL gisfin_0.9.27.tar.gz
+/home/lei/bin/R CMD BATCH document.R
+/home/lei/bin/R CMD build ../../ # --no-build-vignettes
+/home/lei/bin/R CMD check --as-cran gisfin_0.10.9000.tar.gz
+/home/lei/bin/R CMD INSTALL gisfin_0.10.9000.tar.gz

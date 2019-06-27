639560a82117a0d88e7b41a05fc2ebde17d975b3
Markus Kainu <muuankarski@kapsi.fi>
2019-06-27 14:34:36 +0300

    Added a scale argument for get_municipalities
    1000k shape is 2,55MB whereas 4500k (now default) is 350kB. Sharp enough for national level chroropleth maps

--- R/get_municipalities.R
+++ R/get_municipalities.R
@@ -1,20 +1,21 @@
 #' @title Get geospatial data at kunta, maakunta, etc leves from MML
 #' @description preprocessed geospatial data sf-objects
-#' @param year A numeric for year of the administerative borders. Available are 2006, 2010, 2011, 2012, 2014, 2015, 2016, 2017.
+#' @param year A numeric for year of the administerative borders. Available are 2013, 2014, 2015, 2016, 2017, 2018 and 2019.
+#' @param scale A scale or resolution of the shape. Two options: \code{1000} equals 1:1 000 000 and \code{4500} equals 1:4 500 000.
 #' @author Markus Kainu <markus.kainu@kela.fi>
 #' @return sf-object
 #' @examples
 #'  \dontrun{
-#'  f <- get_geo(year=2016,level="kunta")
+#'  f <- get_municipalities(year=2016, scale = 4500)
 #'  plot(f)
 #'  }
 #'
 #' @rdname get_municipalities
 #' @export
 
-get_municipalities <- function(year=2017){
+get_municipalities <- function(year=2017, scale = 4500){
 
-  layer <- paste0("tilastointialueet:kunta1000k_",year)
+  layer <- paste0("tilastointialueet:kunta",scale,"k_",year)
   
   shape <- wfs(url = "http://geo.stat.fi/geoserver/wfs",
                layer = layer,

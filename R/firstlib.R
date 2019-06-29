3ff74df897d5f9a46e286a2fd5270345ad3fcd1e
Joona Lehtomäki <joona.lehtomaki@iki.fi>
2019-06-29 18:55:41 +0300

    Remove old commented out functions

--- R/firstlib.R
+++ R/firstlib.R
@@ -1,12 +1,5 @@
 #' @importFrom ows4R WFSClient
 
-.onAttach <- function(lib, pkg)
-{
-
-  # This may help with encodings in Mac/Linux
-  # Sys.setlocale(locale = "UTF-8")
-  # Sys.setlocale(locale = "WINDOWS-1252")
-
+.onAttach <- function(lib, pkg) {
   packageStartupMessage("\ngisfin R package: tools for open GIS data for Finland.\nPart of rOpenGov <ropengov.github.io>.")
-
 }

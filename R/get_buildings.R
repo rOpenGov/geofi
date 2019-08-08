b67fcd329d56147b5f6b047506d6b949028a7330
Markus Kainu <muuankarski@kapsi.fi>
2019-08-08 11:02:37 +0300

    sketching examples for building data

---
+++ R/get_buildings.R
@@ -0,0 +1,56 @@
+#' @title Get geospatial data with all buildings and electoral districts from Väestörekisterikeskus
+#' @description preprocessed geospatial data sf-objects
+#' @author Markus Kainu <markus.kainu@kela.fi>
+#' @return sf-object
+#' @export
+#' @examples
+#'  \dontrun{
+#'  f <- get_buildings()
+#'  plot(f)
+#'  }
+#'
+#' @rdname get_buildings
+#' @export
+
+get_buildings <- function(){
+  
+  library(dplyr)
+  library(sp)
+  library(sf)
+  tmpfile <- tempfile()
+  tmpdir <- tempdir()
+  download.file("https://www.avoindata.fi/data/dataset/cf9208dc-63a9-44a2-9312-bbd2c3952596/resource/ae13f168-e835-4412-8661-355ea6c4c468/download/suomi_osoitteet_2019-05-15.zip",
+                destfile = tmpfile)
+  unzip(zipfile = tmpfile,
+        exdir = tmpdir)
+  
+  opt <- read.csv(glue::glue("{tmpdir}/Suomi_osoitteet_2019-05-15.OPT"), fileEncoding = "latin1",
+                  sep = ";", 
+                  # nrows = 50000,
+                  stringsAsFactors = FALSE, 
+                  header = FALSE)
+  
+  names(opt) <- c("rakennustu","sijaintiku",
+                  "sijaintima","rakennusty",
+                  "CoordY","CoordX",
+                  "osoitenume", "katunimi_f",
+                  "katunimi_s", "katunumero",
+                  "postinumer", "vaalipiirikoodi",
+                  "vaalipiirinimi","tyhja",
+                  "idx", "date")
+
+  sp.data <- SpatialPointsDataFrame(opt[, c("CoordX", "CoordY")], 
+                                    opt, 
+                                    proj4string = CRS("+init=epsg:3067"))
+  
+  # Project the spatial data to lat/lon
+  # sp.data <- spTransform(sp.data, CRS("+proj=longlat +datum=WGS84"))
+  
+  shape <- st_as_sf(sp.data)
+  
+  # st_coordinates(shape)
+  
+  # shape %>% select(rakennustu) %>% plot()
+  
+  return(shape)
+}

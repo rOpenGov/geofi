62583fc3fd1f9204c54536360fcc2f298a10d57e
Joona Lehtomäki <joona.lehtomaki@iki.fi>
2019-08-17 16:24:33 +0300

    Create test for CRS coercion

--- tests/testthat/test-01-wfs.R
+++ tests/testthat/test-01-wfs.R
@@ -8,9 +8,17 @@ wfs_layer <- paste0("tilastointialueet:kunta", 4500, "k_", 2019)
 
 httptest::with_mock_api({
   test_that("correct data type is returned",{
-    sf_obj <- wfs(url = wfs_url, layer = wfs_layer, 
-                  serviceVersion = wfs_version, logger = NULL)
+    suppressWarnings(sf_obj <- wfs(url = wfs_url, layer = wfs_layer,
+                                   serviceVersion = wfs_version, logger = NULL))
     expect_is(sf_obj, "sf")
+  })
+  test_that("CRS is coerced correctly",{
+    expect_warning(sf_obj <- wfs(url = wfs_url, layer = wfs_layer,
+                                 serviceVersion = wfs_version, logger = NULL),
+                  "Coercing CRS to")
+    suppressWarnings(sf_obj <- wfs(url = wfs_url, layer = wfs_layer,
+                     serviceVersion = wfs_version, logger = NULL))
+    expect_equal(sf::st_crs(sf_obj)$epsg, 3067)
   })
 })
 

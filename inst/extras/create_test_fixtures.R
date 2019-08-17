3c6802b91b91c04b1c41493d48c3fd172d4dd69f
Joona Lehtomäki <joona.lehtomaki@iki.fi>
2019-08-17 17:25:56 +0300

    Move file to inst/extras

--- inst/extras/create_test_fixtures.R
+++ inst/extras/create_test_fixtures.R
@@ -9,6 +9,6 @@ wfs_version <- geofi:::wfs_providers$Tilastokeskus$version
 wfs_layer <- paste0("tilastointialueet:kunta", 4500, "k_", 2019)
 
 httptest::start_capturing()
-wfs(url = wfs_url, layer = wfs_layer, serviceVersion = wfs_version, 
+wfs(url = wfs_url, layer = wfs_layer, serviceVersion = wfs_version,
     logger = NULL)
 httptest::stop_capturing()

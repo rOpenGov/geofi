505362b3e4493ce6129f09b47dbc40c612deaa8a
Joona Lehtomäki <joona.lehtomaki@iki.fi>
2019-08-18 19:46:46 +0300

    Create fixtures for get_municipalities and get_zipcodes

--- inst/extras/create_test_fixtures.R
+++ inst/extras/create_test_fixtures.R
@@ -8,7 +8,6 @@ wfs_url <- geofi:::wfs_providers$Tilastokeskus$URL
 wfs_version <- geofi:::wfs_providers$Tilastokeskus$version
 wfs_layer <- paste0("tilastointialueet:kunta", 4500, "k_", 2019)
 
-
 # test-01-wfs.R -------------------------------------------------------------
 
 httptest::start_capturing()
@@ -16,3 +15,9 @@ get_wfs_layer(url = wfs_url, layer = wfs_layer, serviceVersion = wfs_version,
               logger = NULL)
 httptest::stop_capturing()
 
+# test-02-statfi.R --------------------------------------------------------
+
+httptest::start_capturing()
+get_municipalities(year = 2016, scale = 4500, logger = NULL)
+get_zipcodes(year = 2017, logger = NULL)
+httptest::stop_capturing()

b60b71b3e2d7ffb150e98584ac1d700d294482e7
Joona Lehtomäki <joona.lehtomaki@iki.fi>
2019-08-18 19:47:08 +0300

    Test get_municipalities() and get_zipcodes()

---
+++ tests/testthat/test-02-statfi.R
@@ -0,0 +1,16 @@
+context("Statistics Finland")
+
+httptest::with_mock_api({
+  test_that("correct data type is returned for municipalities",{
+    suppressWarnings({sf_obj <- get_municipalities(year = 2016, scale = 4500,
+                                                   logger = NULL)})
+    expect_is(sf_obj, "sf")
+  })
+  test_that("correct data type is returned for zipcodes",{
+    suppressWarnings({sf_obj <- get_zipcodes(year = 2017, logger = NULL)})
+    expect_is(sf_obj, "sf")
+  })
+ 
+})
+
+

38d6361004a5ca304c07706b814228e671f28811
Joona Lehtomäki <joona.lehtomaki@iki.fi>
2020-01-20 15:44:42 +0200

    Use year 2019

--- tests/testthat/test-01-wfs.R
+++ tests/testthat/test-01-wfs.R
@@ -6,27 +6,20 @@ wfs_version <- geofi:::wfs_providers$Tilastokeskus$version
 wfs_layer <- paste0("tilastointialueet:kunta", 4500, "k_", 2019)
 
 
-# httptest::with_mock_api({
-  # test_that("correct data type is returned",{
-  #   suppressWarnings(sf_obj <- get_wfs_layer(url = wfs_url, layer = wfs_layer,
-  #                                            serviceVersion = wfs_version,
-  #                                            logger = NULL))
-  #   expect_is(sf_obj, "sf")
-  # })
-  
+httptest::with_mock_api({
   test_that("WFS API object is correctly created", {
     expect_error(wfs_api(base_url = "foobar", queries = "foobar"),
                  "^Invalid base URL")
     expect_is(wfs_api(base_url = "http://geo.stat.fi/geoserver/wfs", 
                       queries = append(list("service" = "WFS", "version" = "1.0.0"), 
                                        list(request = "getFeature", 
-                                            typename = "tilastointialueet:kunta4500k_2017"))), 
+                                            typename = "tilastointialueet:kunta4500k_2019"))), 
               "wfs_api")
     # This should work
     expect_is(wfs_api(base_url = "http://geo.stat.fi/geoserver/wfs", 
                       queries = append(list("service" = "WFS", "version" = "1.0.0"), 
                                        list(request = "getFeature", 
-                                            typename = "tilastointialueet:kunta4500k_2017"))), 
+                                            typename = "tilastointialueet:kunta4500k_2019"))), 
               "wfs_api")
   })
   
@@ -40,6 +33,6 @@ wfs_layer <- paste0("tilastointialueet:kunta", 4500, "k_", 2019)
   #                                            logger = NULL))
   #   expect_equal(sf::st_crs(sf_obj)$epsg, 3067)
   # })
-# })
+})
 
 

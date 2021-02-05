8f529ed7917ebb0a95ef9884e21a38f050f94ef0
Pyry Kantanen <pyry.kantanen@utu.fi>
2021-02-05 10:43:30 +0200

    New tests with httptest 4.0.0, over 80 % coverage

--- tests/testthat/test-01-wfs.R
+++ tests/testthat/test-01-wfs.R
@@ -1,39 +1,41 @@
-context("Basic WFS functionality")
+context("Basic WFS functionality1")
 
 # Define a test WFS (Statistics Finland)
 wfs_url <- geofi:::wfs_providers$Tilastokeskus$URL
 wfs_version <- geofi:::wfs_providers$Tilastokeskus$version
-wfs_layer <- paste0("tilastointialueet:kunta", 4500, "k_", 2019)
+wfs_layer <- paste0("tilastointialueet:kunta", 4500, "k_", 2020)
 
+base_queries <- list("service" = "WFS", "version" = wfs_version)
+queries <- append(base_queries, list(request = "getFeature", typename = wfs_layer))
 
-httptest::with_mock_api({
-  test_that("WFS API object is correctly created", {
-    skip_on_cran()
-    expect_error(wfs_api(base_url = "foobar", queries = "foobar"),
-                 "^Invalid base URL")
-    expect_is(wfs_api(base_url = "http://geo.stat.fi/geoserver/wfs",
-                      queries = append(list("service" = "WFS", "version" = "1.0.0"),
-                                       list(request = "getFeature",
-                                            typename = "tilastointialueet:kunta4500k_2019"))),
-              "wfs_api")
-    # This should work
-    expect_is(wfs_api(base_url = "http://geo.stat.fi/geoserver/wfs",
-                      queries = append(list("service" = "WFS", "version" = "1.0.0"),
-                                       list(request = "getFeature",
-                                            typename = "tilastointialueet:kunta4500k_2019"))),
-              "wfs_api")
-  })
-
-  # test_that("CRS is coerced correctly",{
-  #   expect_warning(sf_obj <- get_wfs_layer(url = wfs_url, layer = wfs_layer,
-  #                                          serviceVersion = wfs_version,
-  #                                          logger = NULL),
-  #                 "Coercing CRS to")
-  #   suppressWarnings(sf_obj <- get_wfs_layer(url = wfs_url, layer = wfs_layer,
-  #                                            serviceVersion = wfs_version,
-  #                                            logger = NULL))
-  #   expect_equal(sf::st_crs(sf_obj)$epsg, 3067)
-  # })
+test_that("WFS API object is correctly created", {
+  expect_error(wfs_api(base_url = "foobar", queries = "foobar"),
+               "^Invalid base URL")
 })
 
+httptest::with_mock_dir("wfs_responses",{
+  test_that("WFS API object is correctly created", {
+    skip_on_cran()
+    expect_true(is(wfs_api(base_url = wfs_url, queries), "wfs_api"))
+  })
+})
 
+# clearCache() might is needed when generating new mock files
+# using different year to make sure mock test points to a different file
+
+wfs_layer <- paste0("tilastointialueet:kunta", 4500, "k_", 2019)
+queries <- append(base_queries, list(request = "getFeature", 
+                                     typename = wfs_layer))
+
+# Modified response with $response$status_code changed to 400L
+httptest::with_mock_dir("wfs_modified_responses",{
+  test_that("WFS API object is correctly created", {
+    skip_on_cran()
+    expect_error(wfs_api(base_url = "http://geo.stat.fi/geoserver/wfs", 
+                         queries = queries))
+  })
+})
+
+# httpcache::clearCache()
+# unlink("./tests/testthat/wfs_modified_responses", recursive = TRUE)
+# unlink("./tests/testthat/wfs_responses", recursive = TRUE)
--- tests/testthat/test-02-statfi.R
+++ tests/testthat/test-02-statfi.R
@@ -1,18 +1,32 @@
 context("Statistics Finland")
 
-httptest::with_mock_api({
-
+httptest::with_mock_dir("statfi_responses",{
+  
   test_that("correct data type is returned for municipalities",{
     skip_on_cran()
     suppressWarnings({sf_obj <- get_municipalities(year = 2016, scale = 4500)})
-    expect_is(sf_obj, "sf")
+    expect_true(is(sf_obj, "sf"))
   })
   test_that("correct data type is returned for zipcodes",{
     skip_on_cran()
     suppressWarnings({sf_obj <- get_zipcodes(year = 2017)})
-    expect_is(sf_obj, "sf")
+    expect_true(is(sf_obj, "sf"))
   })
 
+  test_that("correct data type is returned for population grids", {
+    skip_on_cran()
+    suppressWarnings(sf_obj <- get_population_grid(year = 2017, resolution = 5))
+    expect_true(is(sf_obj, "sf"))
+  })
+  
+  test_that("correct data type is returned for statistical grids", {
+    skip_on_cran()
+    suppressWarnings(sf_obj <- get_statistical_grid(resolution = 5, auxiliary_data = TRUE))
+    expect_true(is(sf_obj, "sf"))
+  })
 })
 
+# httpcache::clearCache()
+# remember to also remove subfolder from testthat
+# unlink("./tests/testthat/statfi_responses", recursive = TRUE)
 

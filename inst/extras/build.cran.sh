a2cdd5d8ce4ea8da8b5e8ff637a7926fd1d9bebe
Leo <leo.lahti@iki.fi>
2021-02-15 19:19:04 +0200

    testing

--- inst/extras/build.cran.sh
+++ inst/extras/build.cran.sh
@@ -1,5 +1,5 @@
 #/home/lei/bin/R CMD BATCH document.R
-~/bin/R-4.0.3/bin/R CMD build ../../ --no-build-vignettes
-~/bin/R-4.0.3/bin/R CMD check --as-cran geofi_0.9.2900012.tar.gz
-~/bin/R-4.0.3/bin/R CMD INSTALL geofi_0.9.2900012.tar.gz
+~/bin/R-beta/bin/R CMD build ../../ --no-build-vignettes
+~/bin/R-beta/bin/R CMD check --as-cran geofi_1.0.0.tar.gz
+~/bin/R-beta/bin/R CMD INSTALL geofi_1.0.0.tar.gz
 

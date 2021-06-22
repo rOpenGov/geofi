9ada1c11595b852a170ff97f084decdbe8581f1a
Markus Kainu <markus.kainu@kapsi.fi>
2021-06-22 17:02:09 +0300

    new functions for converting the numerical regions codes in to character of variable width

---
+++ R/convert_codes_as_character.R
@@ -0,0 +1,56 @@
+#' Convert regional codes in on-board municipality key data sets into variable length characters
+#'
+#' Statistics Finland provides numerical codes of regions as two or three digit characters.
+#' By default, those are converted to integers by geofi for convenience, but can be converted back
+#' using this function.
+#'
+#' @param muni_key a municipality key from geofi-package
+#'
+#' @return tibble with codes converted to variable length characters as provided by Statistics Finland
+#'
+#' @author Markus Kainu <markus.kainu@@kapsi.fi>
+#'
+#' @examples
+#'  \dontrun{
+#'  convert_municipality_key_codes(muni_key = geofi::municipality_key)
+#'  }
+#'
+#' @rdname convert_municipality_key_codes
+#' @export
+#'
+convert_municipality_key_codes <- function(muni_key = geofi::municipality_key) {
+
+  # check which columns are integers
+  nums <- unlist(lapply(muni_key, is.integer))
+  nsm <- names(nums[nums])
+  for (i in seq_along(nsm)){
+    muni_key[[nsm[i]]] <- integers_as_character(muni_key, nsm[i])
+  }
+  return(muni_key)
+}
+
+#' Convert an integer variable into character variable of varying width based on variable name.
+#'
+#' @param dat named vector
+#' @param varname named vector
+#'
+#' @note For internal use, not exported.
+#'
+#' @importFrom stringr str_pad
+#'
+#' @author Markus Kainu <markus.kainu@@kapsi.fi>
+#'
+
+integers_as_character <- function(dat, varname){
+  x <- dat[[varname]]
+  if (varname %in% c("municipality_code")){
+    y <- stringr::str_pad(string = x, width = 3, pad = "0")
+    } else if (grepl("kela", varname)){
+      y <- x
+    } else {
+      y <- stringr::str_pad(string = x, width = 2, pad = "0")
+    }
+  return(y)
+  }
+
+
--- R/get_municipalities.R
+++ R/get_municipalities.R
@@ -3,10 +3,11 @@
 #' Thin wrapper around Finnish zip code areas provided by
 #' [Statistics Finland](https://www.stat.fi/org/avoindata/paikkatietoaineistot/kuntapohjaiset_tilastointialueet_en.html).
 #'
-#' @param year A numeric for year of the administerative borders. Available are
+#' @param year A numeric for year of the administrative borders. Available are
 #'             2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020 and 2021.
 #' @param scale A scale or resolution of the shape. Two options: \code{1000}
 #'              equals 1:1 000 000 and \code{4500} equals 1:4 500 000.
+#' @param codes_as_character A logical determining should
 #'
 #' @return sf object
 #'
@@ -56,10 +57,16 @@ Please check your connection, firewall settings and/or review your proxy setting
     library(geofi)
   }
 
+  if (codes_as_character){
+    muni_key <- convert_municipality_key_codes(get(paste0("municipality_key_", year)))
+  } else {
+    muni_key <- get(paste0("municipality_key_", year))
+    }
+
   # Join the attribute data
   sf_obj <- left_join(sf_obj %>%
                         mutate(kunta = as.integer(as.character(.data$kunta))),
-            get(paste0("municipality_key_", year)),
+            muni_key,
             by = c("kunta" = "kunta"))
 
   message("Data is licensed under: ", wfs_providers$Tilastokeskus$license)

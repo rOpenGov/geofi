7d65f3cdf7d92612b13c8291e4a020b15b4a62bf
Markus Kainu <muuankarski@kapsi.fi>
2023-02-14 11:39:26 +0200

    alkuun ogc apin testausta

--- R/check_api_access.R
+++ R/check_api_access.R
@@ -3,23 +3,27 @@
 #' @export
 #' @author Markus Kainu \email{markus.kainu@@kapsi.fi}
 #' @return a logical.
-#' 
+#'
 #' @importFrom httr status_code
 #' @importFrom curl curl_download
-#' 
+#'
 #' @examples
 #'  \dontrun{
 #'    check_api_access()
 #'  }
 
-check_api_access <- function(){ 
-  
+check_api_access <- function(which_api = "statfi_wfs"){
+
   temp <- tempfile()
-  http_url <- "http://geo.stat.fi/geoserver/wfs?service=WFS&version=1.0.0&request=getCapabilities"
-  
+  if (which_api == "statfi_wfs"){
+    http_url <- "http://geo.stat.fi/geoserver/wfs?service=WFS&version=1.0.0&request=getCapabilities"
+  } else if (which_api == "statfi_ogc") {
+    http_url <- "https://geo.stat.fi/inspire/ogc/api/su/?f=json"
+  }
+
   suppressWarnings(
     try(
-      curl::curl_download(http_url, temp, quiet = TRUE), 
+      curl::curl_download(http_url, temp, quiet = TRUE),
       silent = TRUE)
   )
   if (is.na(file.info(temp)$size)) {

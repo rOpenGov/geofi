c5fc15b9307e6fbc421388358a9b8bb588a332f7
Markus Kainu <markus.kainu@kapsi.fi>
2026-04-01 21:58:38 +0300

    version to startupmessage

--- R/firstlib.R
+++ R/firstlib.R
@@ -1,11 +1,6 @@
 
 .onAttach <- function(lib, pkg) {
-     packageStartupMessage("\ngeofi R package: tools for open GIS data for Finland.\nPart of rOpenGov <ropengov.org>.\n
-**************
-Changes in version 1.1.0:
-- Object `municipality_central_localities` is depracated and replaced with function `municipality_central_localities()`. More at https://github.com/rOpenGov/geofi/blob/master/NEWS.md
-- New functions for interacting with both National Land Survey and Statistics Finland OCG API-services. See three new vignettes for examples.
-**************")
+     packageStartupMessage("\ngeofi R package: tools for open GIS data for Finland.\nPart of rOpenGov <ropengov.org>.\nVersion 1.2.1")
 }
 
 # Create an empty environment to hold the WFS provider data
